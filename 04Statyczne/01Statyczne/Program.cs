1216f8b7a86d3dad43ecdc32de2af524bb3c4db4
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-02-10 00:09:50 +0100

    statyczne

---
+++ 04Statyczne/01Statyczne/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Statyczne {
+    class Pierwsza {
+        public static int a = 1;
+        public int b = 2;
+
+        public Pierwsza(int x, int y) {
+            a += x;
+            b += y;
+        }
+        static Pierwsza() {
+            a++;
+            //b++;
+        }
+        public void wypisz() {
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Pierwsza.a = {0}", Pierwsza.a);
+
+            Pierwsza p = new Pierwsza(2, 3);
+            p.wypisz();
+            Console.WriteLine("Pierwsza.a = {0}", Pierwsza.a);
+
+            Pierwsza.a = 19;
+            p.wypisz();
+            Console.WriteLine("Pierwsza.a = {0}", Pierwsza.a);
+
+        }
+    }
+}
---
+++ 04Statyczne/02Rozszerzenie/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02Rozszerzenie {
+    static class RozszerzenieTypuDouble {
+        public static double PotegaCalkowita(this double x, int n) {
+            double iloczyn = 1;
+            for (int i = 0; i < (n > 0 ? n : -n); i++) {
+                iloczyn *= x;
+            }
+            if (n < 0)
+                return 1 / iloczyn;
+            return iloczyn;
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            double x = 10;
+            Console.WriteLine("{0} do potęgi drugiej: {1}", x, x.PotegaCalkowita(2));
+            Console.WriteLine("{0} do potęgi minus drugiej: {1}", 10, (10.0).PotegaCalkowita(-2));
+            Console.ReadKey();
+        }
+    }
+}
