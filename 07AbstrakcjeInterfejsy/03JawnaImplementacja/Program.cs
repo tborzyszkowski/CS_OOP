055ec27b0dd8aeec3908474a5358aa61e73c3d1f
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-02-10 18:51:33 +0100

    klasy abstrakcyjne i interfejsy

---
+++ 07AbstrakcjeInterfejsy/03JawnaImplementacja/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03JawnaImplementacja {
+    interface IPilkarz {
+        void Strzelaj();
+    }
+    interface IZolnierz {
+        void Strzelaj();
+    }
+    class Postac : IZolnierz, IPilkarz {
+        public void Strzelaj() {
+            Console.WriteLine("Strzelam gólnie");
+        }
+        void IPilkarz.Strzelaj() {
+            int x = 1;
+            Console.WriteLine("Strzelam do bramki {0}", x);
+        }
+        void IZolnierz.Strzelaj() {
+            int x = 2;
+            Console.WriteLine("Strzelam do celu {0}", x);
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            Postac p = new Postac();
+            p.Strzelaj();
+            //IPilkarz pp = new Postac();
+            IPilkarz pp = p;
+            pp.Strzelaj();
+            //IZolnierz zp = new Postac();
+            IZolnierz zp = p;
+            zp.Strzelaj();
+
+            (p as IPilkarz).Strzelaj();
+            (p as IZolnierz).Strzelaj();
+        }
+    }
+}
---
+++ 07AbstrakcjeInterfejsy/04WczesnaImplementacja/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04WczesnaImplementacja {
+    interface Interface1 {
+        void f();
+    }
+    class A {
+        public void f() {
+            Console.WriteLine("f() z A");
+        }
+    }
+    class B : A, Interface1 {
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            B b = new B();
+            b.f();
+            ((A)b).f();
+            ((Interface1)b).f();
+        }
+    }
+}
