e4ba255406d3a4c12aedd22d8f717959096b5fcc
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-03-25 10:17:03 +0100

    Przeciążanie lab

---
+++ 05Przeciazanie/Lab/Macierze/Macierz.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Macierze
+{
+    public class Macierz
+    {
+        private double[,] macierz;
+
+        public Macierz(int liczbaWierczy, int liczbaKolumn)
+        {
+            macierz = new double[liczbaWierczy, liczbaKolumn];
+        }
+
+        public double this[int wiersz, int kolumna]
+        {
+            set { macierz[wiersz-1, kolumna-1] = value; }
+            get { return macierz[wiersz-1, kolumna-1]; }
+        }
+
+        public int LiczbaWierszy
+        {
+            get { return macierz.GetLength(0); }
+        }
+        public int LiczbaKolumn
+        {
+            get { return macierz.GetLength(1); }
+        }
+
+        public static Macierz operator +(Macierz a, Macierz b)
+        {
+            if(a.LiczbaWierszy != b.LiczbaWierszy || a.LiczbaKolumn != b.LiczbaKolumn)
+            {
+                throw new ArgumentException("Zły rozmiar macierzy");
+            }
+            Macierz c= new Macierz(a.LiczbaWierszy,a.LiczbaKolumn);
+            for (int i = 0; i < a.LiczbaWierszy; i++)
+            {
+                for (int j = 0; j < a.LiczbaKolumn; j++)
+                {
+                    c.macierz[i, j] = a.macierz[i, j] + b.macierz[i, j];
+                }
+            }
+            return c;
+        }
+
+        public static Macierz operator -(Macierz a, Macierz b)
+        {
+            if (a.LiczbaWierszy != b.LiczbaWierszy || a.LiczbaKolumn != b.LiczbaKolumn)
+            {
+                throw new ArgumentException("Zły rozmiar macierzy");
+            }
+            Macierz c = new Macierz(a.LiczbaWierszy, a.LiczbaKolumn);
+            for (int i = 0; i < a.LiczbaWierszy; i++)
+            {
+                for (int j = 0; j < a.LiczbaKolumn; j++)
+                {
+                    c.macierz[i, j] = a.macierz[i, j] - b.macierz[i, j];
+                }
+            }
+            return c;
+        }
+        public static Macierz operator *(Macierz a, Macierz b)
+        {
+            if (a.LiczbaKolumn != b.LiczbaWierszy)
+            {
+                throw new ArgumentException("Zły rozmiar macierzy");
+            }
+            Macierz c = new Macierz(a.LiczbaWierszy, b.LiczbaKolumn);
+            for (int i = 0; i < a.LiczbaWierszy; i++)
+            {
+                for (int j = 0; j < b.LiczbaKolumn; j++)
+                {
+                    c.macierz[i, j] = 0;
+                    for (int k = 0; k < b.LiczbaWierszy; k++)
+                    {
+                        c.macierz[i, j] += a.macierz[i, k] * b.macierz[k, j];
+                    }
+                }
+            }
+            return c;
+        }
+
+        public static Macierz operator *(double x, Macierz a)
+        {
+            Macierz c = new Macierz(a.LiczbaWierszy, a.LiczbaKolumn);
+            for (int i = 0; i < a.LiczbaWierszy; i++)
+            {
+                for (int j = 0; j < a.LiczbaKolumn; j++)
+                {
+                    c.macierz[i, j] = x * a.macierz[i, j];
+                }
+            }
+            return c;
+        }
+        public static explicit  operator Macierz(double x)
+        {
+            Macierz c = new Macierz(1, 1);
+            c.macierz[0, 0] = x;
+            return c;
+        }
+
+    }
+}
---
+++ 05Przeciazanie/Lab/TestMacierzy/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Macierze;
+
+namespace TestMacierzy
+{
+    static class MacierzDlaKonsoli
+    {
+        public static void WypiszMacierz(this Macierz a)
+        {
+            Console.Write("[ ");
+            for (int i = 1; i <= a.LiczbaWierszy; i++)
+            {
+                Console.Write("<");
+                for (int j = 1; j <= a.LiczbaKolumn; j++)
+                {
+                    Console.Write(a[i, j]);
+                    if (j != a.LiczbaKolumn)
+                        Console.Write("; ");
+                }
+                Console.Write("> ");
+            }
+            Console.Write("]");
+        }
+        private static Random generator = new Random();
+        public static void InicjalizujMacierz(this Macierz a)
+        {
+           
+            for (int i = 1; i <= a.LiczbaWierszy; i++)
+            {                
+                for (int j = 1; j <= a.LiczbaKolumn; j++)
+                {
+                    a[i,j] = generator.Next(0,11);
+                }
+            }
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Macierz a = new Macierz(3, 2), b = new Macierz(3, 2);
+            a.InicjalizujMacierz();
+            Console.WriteLine("Macierz A: ");
+            a.WypiszMacierz();
+            Console.WriteLine("\nMacierz B: ");
+            b.InicjalizujMacierz();
+            b.WypiszMacierz();
+            Console.WriteLine("\nMacierz A + B: ");
+            (a + b).WypiszMacierz();
+            Console.WriteLine("\nMacierz A - B: ");
+            (a - b).WypiszMacierz();
+            Console.WriteLine("\nMacierz 2 * A: ");
+            (2*a).WypiszMacierz();
+            Macierz c = new Macierz(2, 1);
+            c.InicjalizujMacierz();
+            Console.WriteLine("\nMacierz C: ");
+            c.WypiszMacierz();
+            Console.WriteLine("\nMacierz A * C: ");
+            (a * c).WypiszMacierz();
+            Macierz d = (Macierz)33;
+            Console.WriteLine("\nMacierz D: ");
+            d.WypiszMacierz();
+            Console.ReadKey();
+
+        }
+    }
+}
