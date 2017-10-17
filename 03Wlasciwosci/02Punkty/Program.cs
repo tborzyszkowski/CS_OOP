fc1253dc516ad76e469a78139464601d5108766b
tborzyszkowski <t.borzyszkowski@gmail.com>
2017-10-17 13:58:08 +0200

    2017.10.17

--- 03Wlasciwosci/02Punkty/Program.cs
+++ 03Wlasciwosci/02Punkty/Program.cs
@@ -17,12 +17,22 @@ namespace _02Punkty {
                     return y;
                 else return x + y;
             }
+            set {
+                if (nazwaWsp == "x")
+                    x = value;
+                else if (nazwaWsp == "y")
+                    y = value;
+            }
         }
     }
     class Program {
         static void Main(string[] args) {
             Punkt p = new Punkt();
-            Console.WriteLine("x = {0}\ny = {1}\ncos = {2}", p["x"], p["y"], p["z"]);
+            Console.WriteLine("x = {0}\ny = {1}\nz = {2}", p["x"], p["y"], p["z"]);
+            p["x"] = 10;
+            p["y"] = 20;
+            p["z"] = 3000;
+            Console.WriteLine("x = {0}\ny = {1}\nz = {2}", p["x"], p["y"], p["z"]);
         }
     }
 }
