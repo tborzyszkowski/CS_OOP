d11c69a899b5365c030f3218bd4479053b61f3c1
tborzyszkowski <t.borzyszkowski@gmail.com>
2017-10-24 12:16:07 +0200

    beben

---
+++ 06Polimorfizm/lab/beben/beben/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beben
+{
+    public class Beben
+    {
+        String Uderz() { return "Bam Bam !!"; }
+        String ZwrocOpis() { return "To jest bęben."; }
+    }
+}
