e8d17c924eb46435ed7694a6486e19bb8af9c65b
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-03-10 10:29:53 +0100

    Punkt Lab

---
+++ 01Klasy/Lab/Punkt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punkt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Punkt p1 = new Punkt();
+            Punkt p2 = new Punkt();
+
+            p1.x = 3;
+            p1.y = 4;
+            p2.x = 10;
+            p2.y = 20;
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p1.Odleglosc());
+            Console.WriteLine(p2.Odleglosc());
+        }
+    }
+}
---
+++ 01Klasy/Lab/Punkt/Punkt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punkt
+{
+    class Punkt
+    {
+        public double x = 0, y = 0;
+
+        public override String ToString() => $"({x}, {y})";
+
+        public double Odleglosc() => Math.Sqrt(x * x + y * y);
+    }
+}
