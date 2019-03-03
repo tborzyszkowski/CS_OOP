5dd2fe79f0b91e9db8575a603bbfcdab846ee091
tborzyszkowski <t.borzyszkowski@gmail.com>
2019-03-03 08:24:06 +0100

    Punkt

--- 01Klasy/01Punkt/Program.cs
+++ 01Klasy/01Punkt/Program.cs
@@ -8,6 +8,10 @@ namespace _01Punkt {
     class Punkt {
         public double X = 0, Y = 0;
 
+        public Punkt(double x, double y) {
+            this.X = x;
+            this.Y = y;
+        }
         public double dist(Punkt p) {
             return Math.Sqrt((this.X - p.X) * (this.X - p.X) + (this.Y - p.Y) * (this.Y - p.Y));
         }
@@ -18,13 +22,8 @@ namespace _01Punkt {
     }
     class Program {
         static void Main(string[] args) {
-            Punkt p1 = new Punkt();
-            Punkt p2 = new Punkt();
-
-            p1.X = 0;
-            p1.Y = 0;
-            p2.X = 1;
-            p2.Y = 1;
+            Punkt p1 = new Punkt(0, 0);
+            Punkt p2 = new Punkt(1, 1);
 
             Console.WriteLine($"[{p1}, {p2}] = {p1.dist(p2)}");
         }
