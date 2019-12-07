8e46891ed2a5f240f64e9295d1d335cf82b445be
tborzyszkowski <t.borzyszkowski@gmail.com>
2019-12-07 10:10:31 +0100

    Serializacja

--- 14Serializacja/TowarXml/Towar.cs
+++ 14Serializacja/TowarXml/Towar.cs
@@ -7,22 +7,22 @@ using System.Xml.Serialization;
 
 namespace TowarXml
 {
-    public class Towar
-    {
-        [XmlElement("NazwaTowaru")]
-        public string Nazwa { set; get; }
-        public decimal CenaDetaliczna { set; get; }
-        [XmlIgnore]
-        public decimal CenaHurtowa { set; get; }
-        [XmlAttribute("Id")]
-        public int IdTowaru { set; get; }
+	public class Towar
+	{
+		[XmlElement("NazwaTowaru")]
+		public string Nazwa { set; get; }
+		public decimal CenaDetaliczna { set; get; }
+		[XmlIgnore]
+		public decimal CenaHurtowa { set; get; }
+		[XmlAttribute("Id")]
+		public int IdTowaru { set; get; }
 
-        public Towar()
-        {
-            Nazwa = "Brak";
-            CenaDetaliczna = 0;
-            CenaHurtowa = 0;
-            IdTowaru = -1;
-        }
-    }
+		public Towar()
+		{
+			Nazwa = "Brak";
+			CenaDetaliczna = 0;
+			CenaHurtowa = 0;
+			IdTowaru = -1;
+		}
+	}
 }
