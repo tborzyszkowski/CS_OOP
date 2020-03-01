c0c62b730ed6750369fcf840b02ae57dbf7e09e6
tborzyszkowski <t.borzyszkowski@gmail.com>
2020-03-01 11:03:20 +0100

    isTriangle

--- 01Klasy/Lab/Punkt/Program.cs
+++ 01Klasy/Lab/Punkt/Program.cs
@@ -4,20 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Punkt
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Punkt p1 = new Punkt(1, 1);
-            Punkt p2 = new Punkt(2, 2);
+namespace Punkt {
+	class Program {
+		static void Main(string[] args) {
+			Punkt p1 = new Punkt(1, 0);
+			Punkt p2 = new Punkt(2, 0);
 
-            Console.WriteLine(p1);
-            Console.WriteLine(p2);
-            Console.WriteLine(p1.Odleglosc());
-            Console.WriteLine(p2.Odleglosc());
-            Console.WriteLine(p1.OdlegloscDwa(p2));
-        }
-    }
+			Console.WriteLine(p1);
+			Console.WriteLine(p2);
+			Console.WriteLine(p1.Odleglosc());
+			Console.WriteLine(p2.Odleglosc());
+			Console.WriteLine(p1.OdlegloscDwa(p2));
+
+			Console.WriteLine(new Punkt(0, 0).IsTiangle(p1, p2));
+		}
+	}
 }
--- 01Klasy/Lab/Punkt/Punkt.cs
+++ 01Klasy/Lab/Punkt/Punkt.cs
@@ -4,24 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Punkt
-{
-    class Punkt
-    {
-        public double X = 0, Y = 0;
+namespace Punkt {
+	class Punkt {
+		public double X = 0, Y = 0;
 
-        public Punkt(double x, double y) {
-            this.X = x;
-            this.Y = y;
-        }
-        public override String ToString() => $"({X}, {Y})";
+		public Punkt(double x, double y) {
+			this.X = x;
+			this.Y = y;
+		}
+		public override String ToString() => $"({X}, {Y})";
 
-        public double Odleglosc() => Math.Sqrt(X * X + Y * Y);
+		public double Odleglosc() => Math.Sqrt(X * X + Y * Y);
 
-        public double OdlegloscDwa(Punkt p) => 
-            Math.Sqrt(
-                (p.X - this.X) * (p.X - this.X) + 
-                (p.Y - this.Y) * (p.Y - this.Y)
-            );
-    }
+		public double OdlegloscDwa(Punkt p) =>
+			Math.Sqrt(
+				(p.X - this.X) * (p.X - this.X) +
+				(p.Y - this.Y) * (p.Y - this.Y)
+			);
+
+		public bool IsTiangle(Punkt p1, Punkt p2)
+		{
+			var thisP1 = this.OdlegloscDwa(p1);
+			var thisP2 = this.OdlegloscDwa(p2);
+			var p1p2 = p1.OdlegloscDwa(p2);
+
+			return ! (thisP1 + thisP2 <= p1p2) && (thisP1 + p1p2 <= thisP2) && (p1p2 + thisP2 <= thisP1);
+		}
+	}
 }
