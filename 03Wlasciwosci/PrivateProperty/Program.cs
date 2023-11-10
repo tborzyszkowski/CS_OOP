1576001dd2a00e498f5f579ad87f02227b50ba7f
tborzyszkowski <t.borzyszkowski@gmail.com>
2023-11-10 16:42:24 +0100

    private property

---
+++ 03Wlasciwosci/PrivateProperty/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PrivateProperty {
+
+	internal class Property {
+		private int _x { get; public set; } = 0;
+	}
+	internal class Program {
+		static void Main(string[] args) {
+			Property property = new Property();
+			property._x = 1;
+		}
+	}
+}
